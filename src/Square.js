b65987eafc482f3007f4fbabb92d1392398e2b89
Christian <christiandarlington0@gmail.com>
2023-04-18 15:52:46 -0400

    created squares

---
+++ src/Square.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Square = ({value}) => {
+
+  const handleClick = () => {
+    console.log('clicked')
+  }  
+
+  return (
+ <button className="square" onClick={handleClick}>{value}</button>
+  )
+}
+
+export default Square
